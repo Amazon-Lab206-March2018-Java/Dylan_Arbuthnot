ea233a803a27684d21260e7643d91255ff8d5f05
Dylan <darbuthn@amazon.com>
2018-04-25 10:20:15 -0700

    added zookeeper part 2

---
+++ spring/Zoo Keeper/src/Dragon.java
@@ -0,0 +1,18 @@
+
+public class Dragon extends Mammal {
+	public Dragon() {
+		this.setEnergy(200);
+	}
+	public void fly() {
+		this.setEnergy(-50);
+		System.out.println("Whooooosh! The Dragon takes off into the air.");
+	}
+	public void eatHumans( ) {
+		this.setEnergy(25);
+		System.out.println("Arrrrrrrgh! You hear screams in the distance as the Dragon has its meal.");
+	}
+	public void attackTown() {
+		this.setEnergy(-100);
+		System.out.println("Sizzle, sizzle. A fire roars as a town burns in the distance.");
+	}
+}
---
+++ spring/Zoo Keeper/src/DragonTest.java
@@ -0,0 +1,14 @@
+
+public class DragonTest {
+	public static void main(String[] args) {
+		Dragon draco = new Dragon();
+		draco.displayEnergy();
+		draco.attackTown();
+		draco.attackTown();
+		draco.attackTown();
+		draco.eatHumans();
+		draco.eatHumans();
+		draco.fly();
+		draco.fly();
+	}
+}
