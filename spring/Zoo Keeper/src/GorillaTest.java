f97c56c507761ea1028900bafe77d3262f527e02
Dylan <darbuthn@amazon.com>
2018-04-25 10:04:25 -0700

    added spring folder

---
+++ spring/Zoo Keeper/src/GorillaTest.java
@@ -0,0 +1,15 @@
+
+public class GorillaTest {
+	public static void main(String[] args) {
+		Gorilla george = new Gorilla();
+		george.throwSomething();
+		george.throwSomething();
+		george.throwSomething();
+		george.displayEnergy();
+		george.eatBananas();
+		george.eatBananas();
+		george.displayEnergy();
+		george.climb();
+		george.displayEnergy();
+	}
+}
