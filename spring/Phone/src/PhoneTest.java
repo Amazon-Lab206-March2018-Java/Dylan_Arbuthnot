ab87ebeb8a48441ea24f77bdf0f7860f93ac3b8e
Dylan <darbuthn@amazon.com>
2018-04-25 14:11:27 -0700

    added phone

---
+++ spring/Phone/src/PhoneTest.java
@@ -0,0 +1,13 @@
+
+public class PhoneTest {
+	public static void main(String[] args) {
+		Galaxy s9 = new Galaxy ("S9", 99, "Verizon", "*ambient music*");
+		IPhone x = new IPhone("X", 100, "AT&T", "*iphone ringtone*");
+		s9.displayInfo();
+		System.out.println(s9.ring());
+		System.out.println(s9.unlock());
+		x.displayInfo();
+		System.out.println(x.ring());
+		System.out.println(x.unlock());
+	}
+}
