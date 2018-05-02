6a2c682cd65261534f67647873e4fe1ffe8eab6e
Dylan <darbuthn@amazon.com>
2018-05-02 11:32:09 -0700

    added portfolio assignment. not a real portfolio.

---
+++ springProjects/Portfolio/src/main/java/com/darbuth/portfolio/controllers/PortfolioController.java
@@ -0,0 +1,20 @@
+package com.darbuth.portfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PortfolioController {
+	@RequestMapping("/")
+	public String index() {
+		return "forward:/index.html";
+	}
+	@RequestMapping("/about")
+	public String about() {
+		return "forward:/about.html";
+	}
+	@RequestMapping("/projects")
+	public String projects() {
+		return "forward:/projects.html";
+	}
+}
